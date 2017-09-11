58fcc2fd434610bf944e0a63629a11a79c73290e
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-11 10:30:42 -0700

    111
    1111

---
+++ js/bookmarklet_link.js
@@ -0,0 +1,4 @@
+
+javascript:(function(){
+    document.body.appendChild(document.createElement('script')).src='Your Link Goes Here';
+})();
---
+++ js/bookmarklet_template.js
@@ -0,0 +1,41 @@
+console.log('Loaded bookmarklet!!!');
+
+
+javascript:(function(){
+
+
+var v ="2.2.4"; // version of jquery we want to use
+
+if (window.jQuery== undefined || window.jQuery.fn.jquery < v){
+
+    var done = false;
+    var script = document.createElement("script");
+    script.src="http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js"; // load version of jQuery we specify
+    script.onload = script.onreadystatechange = function(){
+
+        if (!done && (!this.readyState || this.readyState=="loaded" || this.readyState =="complete")){
+
+        done = true;
+        initMyBookmarklet(); //If jquery is loaded now run my script
+
+        }
+    };
+document.getElementsByTagName("head")[0].appendChild(script);
+
+
+}else{
+    initMyBookmarklet();
+}
+
+function initMyBookmarklet(){
+    (window.myBookmarklet = function (){
+
+        //YOUR CODE GOES HERE!
+
+
+
+    })();
+
+}
+
+})();
