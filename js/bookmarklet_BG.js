446d33143318b9efd913a652373c8ac58de25651
zye1994 <YoungYe@Youngs-MacBook-Pro.local>
2017-09-11 10:44:39 -0700

    11
    11

---
+++ js/bookmarklet_BG.js
@@ -0,0 +1,43 @@
+console.log('Loaded Background changing bookmarklet!!!');
+
+
+javascript:(function(){
+
+
+var v ="2.2.4"; // version of jquery we want to use
+
+if (window.jQuery== undefined || window.jQuery.fn.jquery < v){
+
+    var done = false;
+    var script = document.createElement("script");
+    script.src="http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js"; // load version of jQuery we specify
+    script.onload = script.onreadystatechange = function(){
+
+        if (!done && (!this.readyState || this.readyState=="loaded" || this.readyState =="complete")){
+
+        done = true;
+        initMyBookmarklet(); //If jquery is loaded now run my script
+
+        }
+    };
+document.getElementsByTagName("head")[0].appendChild(script);
+
+
+}else{
+    initMyBookmarklet();
+}
+
+function initMyBookmarklet(){
+    (window.myBookmarklet = function (){
+
+        //YOUR CODE GOES HERE!
+
+        javascript:void(document.body.style.background = "red";)
+
+
+
+    })();
+
+}
+
+})();
